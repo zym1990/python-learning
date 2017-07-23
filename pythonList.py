31414034fde7a5a04c1a501075e44620a8507c5b
zhangmin <zhangyamin@easy2study.net>
2017-07-23 20:11:09 +0800

    元祖

--- pythonList.py
+++ pythonList.py
@@ -1,3 +1,18 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
+
+#数字列表
+for value in range(1,5):
+    print(value)
+
+
+squares = []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
+for value in range(1,21,2):
+    print(value)
